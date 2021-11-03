9abdc737c8fec76a4b4463e63620eadff2660c8b
Jiamin Ding <jiamin_ding@outlook.com>
2021-11-03 00:21:16 -0700

    Shortest Word Distance

---
+++ src/ShortestWordDistance.java
@@ -0,0 +1,24 @@
+/**
+ * 243. Shortest Word Distance
+ */
+public class ShortestWordDistance {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int lastSeenWord1Index = -1, lastSeenWord2Index = -1;
+        int dist = wordsDict.length;
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
+                lastSeenWord1Index = i;
+                if (lastSeenWord2Index >= 0) {
+                    dist = Math.min(dist, Math.abs(lastSeenWord1Index - lastSeenWord2Index));
+                }
+            } else if (wordsDict[i].equals(word2)) {
+                lastSeenWord2Index = i;
+                if (lastSeenWord1Index >= 0) {
+                    dist = Math.min(dist, Math.abs(lastSeenWord1Index - lastSeenWord2Index));
+                }
+            }
+        }
+
+        return dist;
+    }
+}
